66d0c3a48ef41a4c0cb4f64387ea9852436d0884
Zachary Gutke <zachgutke@gmail.com>
2019-01-28 09:25:19 -0700

    Created base class node

---
+++ DataStructures/Model/Node.hpp
@@ -0,0 +1,45 @@
+//
+//  Node.hpp
+//  DataStructures
+//
+//  Created by Gutke, Zachary on 1/28/19.
+//  Copyright © 2019 Zachary Gutke. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#inlude <assert.h>
+
+template <class Type>
+class Node
+{
+    Type data;
+public:
+    Node(Type data);
+    /* Accessors
+     Type getData();
+     void setData(Type data);
+     */
+};
+
+
+// Template Definitions
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data = data;
+}
+/*
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+template <class Type>
+void Node<Type> :: setData(Type data) {
+    this->data = data;
+}
+*/
+
+#endif /* Node_hpp */
