15ae81f832e144eda23441bed8db3cf6d8e57bee
Zachary Gutke <zachgutke@gmail.com>
2019-02-05 08:36:57 -0700

    Update Controller.cpp

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -12,22 +12,4 @@
 void Controller::start()
 {
     std::cout << "Welcome to the Data Structures App." << std::endl;
-    usingNodes();
 };
-
-void Controller::usingNodes()
-{
-    Node<int> mine(5);
-    Node<std::string> wordHolder("words can be stored too");
-    mine.PrintData();
-    wordHolder.PrintData();
-    
-}
-
-void Controller::usingArrays()
-{
-    Array<int> myArray(5);
-    for (int i = 0; i < myArray.getSize(); i++) {
-        myArray.setAtIndex(i, i);
-    }
-}
